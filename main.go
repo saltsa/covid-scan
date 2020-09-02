4e06cd55645ba3d5b59f8077b470862375058f47
Joonas Aunola <saltsa@gmail.com>
2020-09-02 22:52:54 +0300

    Lots of rewrites, mac support etc

--- main.go
+++ main.go
@@ -2,179 +2,231 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
-	"os"
+	"log"
+	"sort"
 	"sync"
 	"time"
 
 	"github.com/JuulLabs-OSS/ble"
-	log "github.com/sirupsen/logrus"
 )
 
-var adCount uint64
-var measureCount uint64
+// This code bases on goble example code
 
+const minRSSI = -100
+const maxRSSI = -20
+
+// If no beacon, mark O
+var oldEntry = 15 * time.Second
+
+// Removes entry if no beacon
+var cleanEntry = 1 * time.Minute
+
+// flags L in output stats
+var longlivedEntry = 5 * time.Minute
+
+var statsChan chan struct{}
+var statsTicker = time.NewTicker(30 * time.Second)
+var total uint64
+var adCount uint64
 var esService ble.UUID = ble.UUID16(0xfd6f)
 
-// TODO: Fix this beaconinfo and names here
-// and functions
-type beaconInfo struct {
-	RSSI  int
-	Addr  string
-	Count int
+type enEntry struct {
+	RSSI      []int
+	FirstSeen time.Time
+	LastSeen  time.Time
+	Count     int
+	Data      string
 }
 
-type beaconMap map[string]beaconInfo
-type adsPerMinute []beaconMap
+func rssiToRune(rssi int) rune {
+	// level := (rssi - minRSSI) % 8
+	step := (maxRSSI - minRSSI) / 8
+	levelDown := (rssi - minRSSI)
+	level := levelDown / step
+	// levelUp := (maxRSSI - rssi)
+	// level := int(float64(levelDown) / float64(levelDown+levelUp) * 0.8)
+	return rune(0x2581 + level)
+}
 
-var adsSlice adsPerMinute
-var adsMutex sync.Mutex
+func (e *enEntry) String() string {
+	first := time.Since(e.FirstSeen)
+	last := time.Since(e.LastSeen)
+	age := first - last
 
-func (apm adsPerMinute) AddBeacon(data string, rssi int, addr string) {
-	adsMutex.Lock()
+	var avg float64
+	for _, i := range e.RSSI {
+		avg += float64(i)
+	}
+	avg /= float64(len(e.RSSI))
 
-	min := time.Now().Minute()
+	var tooOld = "-"
+	var longLived = "-"
+	if last > oldEntry {
+		tooOld = "O"
+	}
 
-	a, ok := adsSlice[min][data]
-	if !ok {
-		adsSlice[min][data] = beaconInfo{
-			RSSI:  rssi,
-			Count: 1,
-			Addr:  addr,
-		}
-	} else {
-		a.Count++
-		if a.RSSI < rssi {
-			a.RSSI = rssi
+	if age > longlivedEntry {
+		longLived = "L"
+	}
+
+	rssiS := make([]rune, 60)
+	for i := 0; i < len(rssiS); i++ {
+		idxOut := len(rssiS) - i - 1
+		idxIn := len(e.RSSI) - 1 - i
+		if idxOut >= 0 {
+			if idxIn >= 0 {
+				rssiS[idxOut] = rssiToRune(e.RSSI[idxIn])
+			} else {
+				rssiS[idxOut] = rune(0x20)
+			}
 		}
 	}
 
-	adsMutex.Unlock()
+	return fmt.Sprintf("%s%s rssi=[%s] last=%s age=%s first=%s count=%d rssi avg=%f latest=%d", tooOld, longLived, string(rssiS), last, first, age, e.Count, avg, e.RSSI[len(e.RSSI)-1])
 }
 
-func (apm adsPerMinute) Print() {
-	adsMutex.Lock()
-
-	min := time.Now().Minute()
-
-	log.Infof("Ads per minute")
-	for i := (min + 60 - 20); i <= (min + 60); i++ {
-		// log.Debugf("i is %d", i)
-		idx := i % 60
-		val := adsSlice[idx]
-
-		var rssiMin = -110
-		var rssiMax = -10
-		// for _, adv := range val {
-		// 	if adv.RSSI < rssiMin {
-		// 		rssiMin = adv.RSSI
-		// 	}
-		// 	if adv.RSSI > rssiMax {
-		// 		rssiMax = adv.RSSI
-		// 	}
-		// }
-
-		if rssiMin >= rssiMax {
-			fmt.Printf("%02d: %3d\n", idx, len(val))
-			continue
-		}
-		stepLen := rssiMax - rssiMin
-		// log.Debugf("max=%d min=%d steplen is %d", rssiMax, rssiMin, stepLen)
-		metrics := make([]rune, stepLen+1)
-
-		for r := 0; r <= stepLen; r++ {
-			var c int
-			for _, j := range val {
-				if j.RSSI == rssiMin+r {
-					c++
-				}
-			}
-			if c > 8 {
-				c = 8
-			}
-
-			// U+2581 - U+2588
-			metric := rune(0x2580 + c)
-			if c == 0 {
-				metric = ' '
-			}
-			metrics[r] = metric
-		}
-		fmt.Printf("%02d: %3d %4d [%s] %4d\n", idx, len(val), rssiMin, string(metrics), rssiMax)
+func (e *enEntry) Expired() bool {
+	if time.Since(e.LastSeen) > cleanEntry {
+		return true
 	}
-	adsMutex.Unlock()
+	return false
 }
 
-var fout *os.File
+var dataMap map[string]int
+var srcMap map[string]*enEntry
+var mutex sync.Mutex
 
-func toFile(data string) {
-	_, err := fout.WriteString(data + "\n")
-	if err != nil {
-		log.Fatalln(err)
-	}
-}
 func init() {
-	is := time.Now()
-	adsSlice = make([]beaconMap, 60)
-	for i := range adsSlice {
-		adsSlice[i] = make(beaconMap)
-	}
-	adsMutex = sync.Mutex{}
-	var err error
-	fout, err = os.OpenFile("covid.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Infof("data structures built in %s", time.Since(is))
+	dataMap = make(map[string]int)
+	srcMap = make(map[string]*enEntry)
+
+	statsChan = make(chan struct{})
 }
+
+func Add(srcs string, data []byte, rssi int) {
+	mutex.Lock()
+	var found bool
+	var new bool
+
+	// if rssi < -70 {
+	// 	return
+	// }
+	// log.Println("RSSI:", rssi)
+	datas := fmt.Sprintf("%x", data)
+	_, found = dataMap[datas]
+	if !found {
+		dataMap[datas] = 1
+		new = true
+	} else {
+		dataMap[datas]++
+	}
+
+	ent, found := srcMap[srcs]
+	if !found {
+		ent = &enEntry{
+			FirstSeen: time.Now(),
+			RSSI:      make([]int, 0),
+			Data:      datas,
+		}
+		srcMap[srcs] = ent
+		new = true
+	} else {
+		if ent.Data != datas {
+			log.Printf("data differs in saved entity: %s vs. %s", ent.Data, datas)
+		}
+	}
+
+	ent.Count++
+	ent.LastSeen = time.Now()
+	ent.RSSI = append(ent.RSSI, rssi)
+	total++
+
+	mutex.Unlock()
+
+	if new {
+		statsChan <- struct{}{}
+	} else if total%10 == 0 {
+		statsChan <- struct{}{}
+	}
+}
+
+func stats() {
+	for {
+		mutex.Lock()
+
+		var keys []string
+		for key := range srcMap {
+			keys = append(keys, key)
+		}
+
+		sort.Slice(keys, func(i, j int) bool {
+			ar := srcMap[keys[i]].RSSI
+			br := srcMap[keys[j]].RSSI
+
+			a := ar[len(ar)-1]
+			b := br[len(br)-1]
+			return a < b
+		})
+
+		fmt.Printf("List of found beacons at %s\n", time.Now().Format("15:04:05"))
+		for _, key := range keys {
+			data := srcMap[key]
+			fmt.Printf("%s\n", data)
+			if data.Expired() {
+				delete(srcMap, key)
+			}
+		}
+		fmt.Println("end of list")
+		fmt.Println()
+
+		mutex.Unlock()
+
+		select {
+		case <-statsChan:
+		case <-statsTicker.C:
+		}
+	}
+}
+
 func hand(a ble.Advertisement) {
 	adCount++
 
 	addr := a.Addr()
 	svcData := a.ServiceData()
 
-	// log.Debugf("tx power: %d", a.TxPowerLevel())
 	for _, svc := range svcData {
 		if svc.UUID.Equal(esService) {
-			measureCount++
-			ds := fmt.Sprintf("%X", svc.Data)
-			as := fmt.Sprintf("%X", addr)
-			dout := fmt.Sprintf("%d,%s,%s,%d", time.Now().Unix(), as, ds, a.RSSI())
-			toFile(dout)
-			adsSlice.AddBeacon(ds, a.RSSI(), as)
+			Add(addr.String(), svc.Data, a.RSSI())
 		}
 	}
 
 }
 
-func scan() {
+func scan(dups bool) {
 	ctx := context.Background()
 
-	err := ble.Scan(ctx, true, hand, nil)
+	err := ble.Scan(ctx, dups, hand, nil)
 
 	if err != nil {
 		log.Fatalln("scan error:", err)
 	}
 }
 
-func printStats() {
-	go func() {
-		for {
-			log.Printf("measures=%d, total_ads=%d", measureCount, adCount)
-			adsSlice.Print()
-			time.Sleep(15 * time.Second)
-		}
-	}()
-}
-
 func main() {
-	log.SetLevel(log.DebugLevel)
-	log.Debug("init")
+	dups := flag.Bool("allow-duplicates", true, "allow duplicates when scanning")
+	flag.Parse()
+
 	dev, err := getDev()
 	if err != nil {
-		log.Fatalln("got error:", err)
+		log.Fatalln("get dev", err)
 	}
 	ble.SetDefaultDevice(dev)
-	printStats()
-	scan()
+	go stats()
+
+	log.Println("start scan")
+	scan(*dups)
+	log.Println("scan exit")
+
 }
